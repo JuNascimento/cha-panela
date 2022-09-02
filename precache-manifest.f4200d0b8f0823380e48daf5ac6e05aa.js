06bd19e195e42f9981e679d28de14b8581f17eb5
Julia Nascimento <ju.araujo11@yahoo.com.br>
2022-09-02 15:15:32 -0300

    Updates

---
+++ precache-manifest.f4200d0b8f0823380e48daf5ac6e05aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "92f16180ecb8292b504e",
+    "url": "/cha-panela/static/css/main.faa6b8df.chunk.css"
+  },
+  {
+    "revision": "92f16180ecb8292b504e",
+    "url": "/cha-panela/static/js/main.899e76a0.chunk.js"
+  },
+  {
+    "revision": "803b421dfd6e954ecb2c",
+    "url": "/cha-panela/static/js/runtime~main.f08000ce.js"
+  },
+  {
+    "revision": "9b1483201004340eb23f",
+    "url": "/cha-panela/static/js/2.abc239e4.chunk.js"
+  },
+  {
+    "revision": "d14e4b2bc92b6ae455e9",
+    "url": "/cha-panela/static/js/3.4ea34601.chunk.js"
+  },
+  {
+    "revision": "581bdd99582bd136b5a9363977f0f262",
+    "url": "/cha-panela/static/media/juliaepedro.581bdd99.png"
+  },
+  {
+    "revision": "8cc403a6841693a60674883b10ba8450",
+    "url": "/cha-panela/static/media/utilidades.8cc403a6.png"
+  },
+  {
+    "revision": "fc38d8148a0f984eb1bb8f5c24d31bd8",
+    "url": "/cha-panela/static/media/flor12.fc38d814.png"
+  },
+  {
+    "revision": "bd6a66c2a868e797884fa542625b0e86",
+    "url": "/cha-panela/index.html"
+  }
+];
